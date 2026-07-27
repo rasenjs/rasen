b611bda1bf701b1c9d4c02b939a801c59ca56a0d
Harvey Woo <harvey.woo@outlook.com>
2026-07-27 18:42:03 +0800

    ⬆️(examples) upgrade to react-native 0.86.0
    - Regenerated ios/ and android/ from clean RN 0.86 template
    - Updated react-native, react, community CLI, and @react-native/* deps
    - Removed @types/react-native (built-in types in RN 0.86)
    - Fixed monorepo hoisting: removed stale dep causing wrong Metro version
    - Added Gemfile, .prettierrc.js, .watchmanconfig from template
    - iOS: Swift AppDelegate with RCTReactNativeFactory (no post_install patch)
    - Android: Kotlin MainActivity/MainApplication with new arch enabled

--- examples/vue-rn-test/android/app/src/main/java/com/vuerntest/MainActivity.kt
+++ examples/vue-rn-test/android/app/src/main/java/com/vuerntest/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rasenexample
+package com.vuerntest
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "RasenExample"
+  override fun getMainComponentName(): String = "VueRnTest"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
